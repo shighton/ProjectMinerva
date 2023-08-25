0f0c9069a86b68b29befed4b0d0cba3855061d93
shighton <dime.bastian@gmail.com>
2023-08-24 20:14:08 -0400

    English words to hashes and placed in a dataframe along with the original words.

--- Minerva.py
+++ Minerva.py
@@ -5,5 +5,9 @@ import tensorflow as tf
 with open('Minerva.npy', 'rb') as file:
     output = np.load(file, allow_pickle=True)
 
-# print(output)
+print(output)
+
+# for label, feature in output:
+#     print("Txt: ", label)
+#     print("Hash: ", feature)
 
